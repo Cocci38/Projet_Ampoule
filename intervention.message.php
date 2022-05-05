806e1c34603545e6621afb00f769ebe886daea60
Sandra <sandra.villot038@gmail.com>
2022-05-05 14:12:02 +0200

    correction bug message

--- intervention.message.php
+++ intervention.message.php
@@ -21,7 +21,7 @@ require_once './crud/connexion.php';
 if(isset($envoyer)&& !empty(trim($recherche))){
 $sql4=$conn->prepare("SELECT created_at, message, date, username FROM messages
 JOIN ampoule ON messages.id_message = ampoule.message_id
-JOIN users ON messages.user_id = users.id
+JOIN users ON messages.user_id = users.id_user
 WHERE MATCH (message) AGAINST ( ' $recherche ' )");
 $sql4->execute();
 $resultat4=$sql4->fetchAll(PDO::FETCH_ASSOC);
@@ -50,7 +50,7 @@ $afficher='oui';
                 <tr>
                     <th>Date du message</th>
                     <th>Message</th>
-                    <th>Auteur</th>
+                    <th>Gardien</th>
                     <th>Date de l'intervention</th>
                 </tr>
             </thead>
